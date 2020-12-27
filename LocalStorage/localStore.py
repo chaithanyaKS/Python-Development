99b1f9cc0a3e282020e3d454199530eb270880b9
chaithanyaKS <chaithanya2934@gmail.com>
2020-12-27 23:50:15 +0530

    fixed small bugs

--- LocalStorage/localStore.py
+++ LocalStorage/localStore.py
@@ -1,26 +1,26 @@
 import pathlib
 import json
 import os
-from typing import cast
+
+
 BASE_FILE_PATH = pathlib.Path.cwd()
 STORE_FILE_PATH = BASE_FILE_PATH / 'store' / 'store.json'
-STORE_INDEX_PATH = BASE_FILE_PATH / 'store' / 'keys.txt'
 
 
 class LocalSore:
+    __BASE_FILE_PATH = pathlib.Path.cwd()
+    __STORE_FILE_PATH = __BASE_FILE_PATH / 'store' / 'store.json'
 
-    def __init__(self, store_file_path=STORE_FILE_PATH, store_index_path=STORE_INDEX_PATH):
+    def __init__(self, store_file_path=__STORE_FILE_PATH):
         self.store_file_path = pathlib.Path(store_file_path)
-        self.store_index_path = pathlib.Path(store_index_path)
-        self.check_or_create_files()
-        self.keys = self.get_keys()
+        self.__BASE_PATH = self.store_file_path.parent
+        self.store_keys_path = self.__BASE_PATH / 'keys.txt'
+        self.__check_or_create_files()
+        self.keys = self.__get_keys()
 
-    def check_or_create_files(self):
-        if not self.store_file_path.parent.exists():
-            os.makedirs(self.store_file_path.parent)
-
-        if not self.store_index_path.parent.exists():
-            os.makedirs(self.store_index_path.parent)
+    def __check_or_create_files(self):
+        if not self.__BASE_PATH.exists():
+            os.makedirs(self.__BASE_PATH)
 
         if not self.store_file_path.exists():
             with open(self.store_file_path, 'w'):
@@ -28,89 +28,92 @@ class LocalSore:
             print(
                 f'Created Store at {self.store_file_path.absolute()}')
 
-        if not self.store_index_path.exists():
-            with open(self.store_index_path, 'w'):
+        if not self.store_keys_path.exists():
+            with open(self.store_keys_path, 'w'):
                 pass
             print(
-                f'Created Index File at {self.store_index_path.absolute()}')
+                f'Created Index File at {self.store_keys_path.absolute()}')
 
-    def get_keys(self):
-        with open(self.store_index_path) as fp:
+    def __get_keys(self):
+        with open(self.store_keys_path) as fp:
             keys = fp.readline().split(',')
-            return keys if keys[0] != '' else None
+            return keys if keys[0] != '' else []
 
-    def check_key(self, key):
-        if self.keys == None:
-            return False
+    def __check_key(self, key):
         return True if key in self.keys else False
 
-    def get_store(self):
+    def __get_store(self):
+        if self.keys == []:
+            raise Exception('No Data is present in Store')
         with open(self.store_file_path, 'r') as fp:
             return json.load(fp)
 
-    def update_keys(self, key, op='add'):
+    def __update_keys(self, key, op='add'):
         if op == 'del':
             self.keys.remove(key)
-            with open(self.store_index_path, 'w') as fp:
-                fp.write(*self.keys)
+            with open(self.store_keys_path, 'w') as fp:
+                fp.write(','.join(self.keys).lstrip(','))
         else:
-            if self.check_key(key):
+            if self.__check_key(key):
                 raise Exception('Key already exists')
 
-            separator = ',' if self.keys is not None else ''
+            self.keys.append(key)
 
-            with open(self.store_index_path, 'a') as fp:
-                fp.write(f'{separator}{key}')
+            with open(self.store_keys_path, 'w') as fp:
+                fp.write(','.join(self.keys))
 
-            if self.keys == None:
-                self.keys == [key]
-            else:
-                self.keys.append(key)
-
-    def update_store(self, store):
+    def __update_store(self, store):
         with open(self.store_file_path, 'w') as fp:
             json.dump(store, fp)
 
     def print_store(self):
-        store = self.get_store()
-
-        print('='*50)
-        print('key        data')
-        print('='*50)
-        print()
-        for key, data in store.items():
-            print(f'{key}    :    {data}', end='\n\n')
-        print('='*50)
+        store = self.__get_store()
+        if self.keys == None:
+            print('No data is present in the store')
+        else:
+            print('='*50)
+            print('key        data')
+            print('='*50)
+            print()
+            for key, data in store.items():
+                print(f'{key}    :    {data}', end='\n\n')
+            print('='*50)
 
     def write(self, key,  data):
-        if self.check_key(key):
+        if self.__check_key(key):
             raise Exception('Key Already Exists')
         else:
             try:
-                store = self.get_store()
+                store = self.__get_store()
                 store[key] = data
             except:
                 store = {key: data}
-            self.update_store(store)
-            self.update_keys(key)
+            self.__update_store(store)
+            self.__update_keys(key)
             print('Data Stored Successfully', end='\n\n')
 
     def read(self, key):
-        if key not in self.keys:
+        if not self.__check_key(key):
             raise Exception('Key Not Found')
-        store = self.get_store()
-        return store[key]
+        try:
+            store = self.__get_store()
+            return store[key]
+        except Exception as e:
+            print(e)
 
     def delete(self, key):
-        if key not in self.keys:
+        if not self.__check_key(key):
             raise Exception('Key Not Found')
-        store = self.get_store()
-        del store[key]
-        self.update_store(store)
-        self.update_keys(key, op='del')
-        print(self.keys)
-        print('Data Deleted Successfully')
+        try:
+            store = self.__get_store()
+            del store[key]
+            self.__update_store(store)
+            self.__update_keys(key, op='del')
+            print(self.keys)
+            print('Data Deleted Successfully')
+
+        except Exception as e:
+            print(e)
 
 
-store = LocalSore('stores/store.json', 'stores/keys.txt')
-store.write("3", 'asdsa')
+store = LocalSore('stores/store.json')
